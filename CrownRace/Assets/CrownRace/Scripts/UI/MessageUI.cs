9184b05cf0d5177c2130f52138a6adeb30a67104
zhangming <584917502@qq.com>
2017-02-04 21:39:57 +0800

    add MessageUI

---
+++ CrownRace/Assets/CrownRace/Scripts/UI/MessageUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public delegate void VoidEvent();
+public class MessageUI : MonoBehaviour {
+	[SerializeField]
+	private Text m_message;
+	private VoidEvent m_callback;
+	public void ShowMessage(string message, float seconds, VoidEvent callback=null)
+	{
+		gameObject.SetActive(true);
+		m_message.text = message;
+		m_callback = callback;
+		StartCoroutine(HideSelf(seconds));
+	}
+	IEnumerator HideSelf(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		gameObject.SetActive(false);
+		if(null != m_callback)
+		{
+			m_callback.Invoke();
+		}
+	}
+}
